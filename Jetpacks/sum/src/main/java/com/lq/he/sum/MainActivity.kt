1b3bab050668a44060c891630c0ef31e6892ad87
rlq <rlqjue@gmail.com>
2020-06-02 09:37:31 +0800

    kotlin jetpack

---
+++ Jetpacks/sum/src/main/java/com/lq/he/sum/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lq.he.sum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView<ActivityGardenBinding>(this, R.layout.activity_garden)
+    }
+}
