873424d15d8ea31a2508450f7f05bc2a5de28c1f
rlq <rlqjue@gmail.com>
2020-07-03 11:45:37 +0800

    plantings

--- Jetpacks/sum/src/main/java/com/lq/he/sum/data/PlantAndGardenPlantings.kt
+++ Jetpacks/sum/src/main/java/com/lq/he/sum/data/PlantAndGardenPlantings.kt
@@ -3,7 +3,7 @@ package com.lq.he.sum.data
 import androidx.room.Embedded
 import androidx.room.Relation
 
-
+// 数据类，连接VM与DB
 data class PlantAndGardenPlantings(
     @Embedded
     val plant: Plant,
