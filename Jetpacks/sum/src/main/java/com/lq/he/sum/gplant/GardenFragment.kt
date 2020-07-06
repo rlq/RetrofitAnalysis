ecfa255973fcde4c06f3b51231f4f1b605e61743
rlq <rlqjue@gmail.com>
2020-07-06 08:10:24 +0800

    Fragment

--- Jetpacks/sum/src/main/java/com/lq/he/sum/gplant/GardenFragment.kt
+++ Jetpacks/sum/src/main/java/com/lq/he/sum/gplant/GardenFragment.kt
@@ -1,4 +1,4 @@
-package com.lq.he.sum
+package com.lq.he.sum.gplant
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,12 +8,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.viewpager2.widget.ViewPager2
-import com.lq.he.sum.adapters.GardenPlantingAdapter
-import com.lq.he.sum.adapters.PLANT_LIST_PAGE_INDEX
+import com.lq.he.sum.GardenPlantManager
+import com.lq.he.sum.R
+import com.lq.he.sum.gplant.ad.GardenPlantingAdapter
+import com.lq.he.sum.home.adapters.PLANT_LIST_PAGE_INDEX
 import com.lq.he.sum.databinding.FragmentGardenBinding
-import com.lq.he.sum.viewmodels.GardenPlantingListViewModel
+import com.lq.he.sum.gplant.vm.GardenPlantingListViewModel
 
 /**
+ * 我的花园的Fragment
  * VM + observe
  * LiveData
  * binding
@@ -24,8 +27,11 @@ class GardenFragment : Fragment() {
     // suspend
     private lateinit var binding: FragmentGardenBinding
 
+    // 加载数据
     private val viewModel: GardenPlantingListViewModel by viewModels {
-        GardenPlantManager.provideGardenPlantingListVieModelFactory(requireContext())
+        GardenPlantManager.provideGardenPlantingListVieModelFactory(
+            requireContext()
+        )
     }
 
     override fun onCreateView(
@@ -43,7 +49,7 @@ class GardenFragment : Fragment() {
         return binding.root
     }
 
-    // 点击跳转到下一页
+    // 添加植物，点击跳转到植物目录页，然后打开详情点击+
     private fun navigateToPlantListPage() {
         requireActivity().findViewById<ViewPager2>(R.id.view_pager).currentItem = PLANT_LIST_PAGE_INDEX
     }
