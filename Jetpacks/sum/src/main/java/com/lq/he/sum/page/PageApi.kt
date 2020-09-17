855f4235c0e2483a8312af19d25aeab3ab04378c
rlq <rlqjue@gmail.com>
2020-09-17 16:48:49 +0800

    retrofit api

--- Jetpacks/sum/src/main/java/com/lq/he/sum/page/PageApi.kt
+++ Jetpacks/sum/src/main/java/com/lq/he/sum/page/PageApi.kt
@@ -1,8 +1,12 @@
 package com.lq.he.sum.page
 
 import com.lq.he.sum.data.Plant
+import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface PageApi {
@@ -10,4 +14,7 @@ interface PageApi {
     @GET("page/view")
     fun getPlants(@Query("start") since: Int,
                   @Query("count") perpage: Int) : Call<Plant>
+
+    @POST("user/login")
+    fun login(@Body body: RequestBody) : Call<Response<Plant>>
 }
